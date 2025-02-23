f28fc86a5dd01ce2751ca1ff9e8dde1bea00b2bd
LoMein123 <jbnouse@gmail.com>
2025-02-23 00:13:57 -0500

    Added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,45 @@ import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import ScrollToTop from './components/ScrollToTop';
 
+import { useEffect, useState } from 'react';
+import PuffLoader from 'react-spinners/PuffLoader';
+import { motion } from "framer-motion";
+
 function App() {
-  return (
-    <div>
-      <Landing />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
-      <ScrollToTop/>
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+
+    // Loading screen for 2 seconds
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 750);
+    }, []);
+
+    if (loading) {
+        return (<div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+                    <PuffLoader
+                        color="#faf6e5"
+                        size={200}
+                    />
+                </div>
+        );
+    }
+
+    return (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3 }}
+          className="text-center"
+        >
+            <Landing />
+            <About />
+            <Experience />
+            <Projects />
+            <Contact />
+            <ScrollToTop/>
+        </motion.div>
+    );
 }
 
 export default App;
