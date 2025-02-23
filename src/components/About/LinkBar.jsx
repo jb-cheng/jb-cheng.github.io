7521a96907a7c053950a65007c05474c1c3b4994
LoMein123 <jbnouse@gmail.com>
2025-02-23 00:33:16 -0500

    Added background pattern

--- src/components/About/LinkBar.jsx
+++ src/components/About/LinkBar.jsx
@@ -1,7 +1,7 @@
 export default function LinkBar() {
     return (
         <div className='link-bar'>
-            <div className='link-btn' onClick={() => {window.open("https://www.google.com")}}>Resume</div>
+            <div className='link-btn' onClick={() => {window.open("https://drive.google.com/file/d/1Q-JqmP3gyhAw0awH96Ld_qf_WJIlSKRX/view?usp=sharing")}}>Resume</div>
             <div className='link-btn' onClick={() => {window.open("https://www.linkedin.com/in/jb-cheng/")}}>LinkedIn</div>
             <div className='link-btn' onClick={() => {window.open("https://github.com/jb-cheng")}}>GitHub</div>
         </div>
