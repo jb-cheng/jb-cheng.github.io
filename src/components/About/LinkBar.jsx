b4e78471025b9cb3410cc71afc4066c06377c5ab
LoMein123 <jbnouse@gmail.com>
2025-10-13 21:24:26 -0400

    resume link

--- src/components/About/LinkBar.jsx
+++ src/components/About/LinkBar.jsx
@@ -10,7 +10,7 @@ export default function LinkBar() {
       <img src={Linkedin} alt="LinkedIn Icon" onClick={() => {window.open("https://www.linkedin.com/in/jb-cheng/")}}/>
       <img src={Github} alt="GitHub Icon" onClick={() => {window.open("https://github.com/jb-cheng")}}/>
       <img src={Email} alt="Email Icon" onClick={() => window.location = 'mailto:junbin.cheng@ontariotechu.net'}/>
-      <div className='resume-btn' onClick={() => {window.open("https://drive.google.com/file/d/1uVpb3O3t0P0xEpFoOboiMue5ZQQycqUF/view?usp=sharing")}}>Resume</div>
+      <div className='resume-btn' onClick={() => {window.open("https://drive.google.com/file/d/1ZBqbd-xVhR0zyx5hK__pfPcM2yewuKAx/view?usp=sharing")}}>Resume</div>
     </div>
   );
 };
